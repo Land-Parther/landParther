7f9f53523046708614ae7c3a9f052e4ae8d63828
Jimin <chplatform.jimin@gmail.com>
2023-10-16 10:29:27 +0900

    [feat] FrontEnd 초기 프로젝트 생성

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import GlobalStyles from "@/styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
